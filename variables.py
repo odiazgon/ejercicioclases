74c438cd09afa3f91c5cb11a5effb2d76f86d00b
Tavinho <odiazgon@cisco.com>
2020-02-14 14:19:22 -0600

    actualizacion

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 import requests
 import json
-token = 'OTg4NzBhNjItZmZhYy00MzFhLTk0ODItMGZmY2M5YWFiZjk1ZDgwMmI3M2EtNmEy_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
+token = 'insert_token'
 urlteams = 'https://api.ciscospark.com/v1/messages'
 Line_spacing=""
 tipomensaje = input("Ingresa el formato de mensaje (xml/yaml) : ")
